fff3876fe623df4c455e3186a40da6f52b7fc783
Lucas Lomeu <lucaslomeugomes@gmail.com>
2024-07-24 18:42:33 -0300

    [FEAT] HTTP Provider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SpotifyService } from './core/services/spotify/spotify.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'playlistConverter';
+
+  constructor(private spotifyService: SpotifyService) {}
+  ngOnInit() {
+    this.spotifyService
+      .authenticate()
+      .subscribe((data) =>
+        this.spotifyService
+          .getPlaylists()
+          .subscribe((res) => console.warn('RES', res))
+      );
+  }
 }
