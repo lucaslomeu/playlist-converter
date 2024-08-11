3aa40f563e0252ca88759b71c452ac8e54a80cae
Lucas Lomeu <lucaslomeugomes@gmail.com>
2024-08-11 11:14:28 -0300

    [FEAT] Initialize first page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SpotifyService } from './core/services/spotify/spotify.service';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import {
+  AuthenticateResponse,
+  SpotifyService,
+} from './core/services/spotify/spotify.service';
+import { YoutubeService } from './core/services/youtube/youtube.service';
 
 @Component({
   selector: 'app-root',
@@ -12,14 +16,70 @@ import { SpotifyService } from './core/services/spotify/spotify.service';
 export class AppComponent implements OnInit {
   title = 'playlistConverter';
 
-  constructor(private spotifyService: SpotifyService) {}
+  playlistsInput: any;
+
+  headphoneImage = 'public/images/headphone.png';
+
+  constructor(
+    private spotifyService: SpotifyService,
+    private youtubeService: YoutubeService,
+    private route: ActivatedRoute
+  ) {}
   ngOnInit() {
-    this.spotifyService
-      .authenticate()
-      .subscribe((data) =>
+    //  this.spotify()
+    // this.youtube();
+
+    this.route.queryParams.subscribe((params) => {
+      const code = params['code'];
+      if (code) {
         this.spotifyService
-          .getPlaylists()
-          .subscribe((res) => console.warn('RES', res))
-      );
+          .getAccessToken(code)
+          .subscribe((res: AuthenticateResponse) => {
+            console.warn('RES', res);
+            this.spotifyService
+              .getPlaylists(res.access_token)
+              .subscribe((res) => {
+                console.warn('RES', res);
+              });
+          });
+      } else {
+        // Lidar com o erro ou redirecionar
+      }
+    });
+
+    this.route.fragment.subscribe((fragment: string | null) => {
+      if (fragment) {
+        const params = new URLSearchParams(fragment);
+        const accessToken = params.get('access_token');
+        const authCode = params.get('code');
+
+        if (accessToken) {
+          console.log('Access Token:', accessToken);
+          this.youtubeService.getPlaylists(accessToken).subscribe((res) => {
+            console.warn('PLAYLISTTTT', res);
+          });
+          // Aqui você pode armazenar o token e usá-lo para futuras requisições à API do YouTube
+        }
+
+        if (authCode) {
+          console.log('Authorization Code:', authCode);
+          // Aqui você pode enviar o código ao seu servidor para trocá-lo por um token
+        }
+      }
+    });
+  }
+
+  protected authenticateSpotify() {
+    // this.spotifyService.authenticate().subscribe((data) => {
+    //   this.spotifyService.getPlaylists(data).subscribe((res) => {
+    //     console.warn('RES', res);
+    //   });
+    // });
+
+    this.spotifyService.authenticate();
+  }
+
+  protected youtube() {
+    this.youtubeService.authenticate();
   }
 }
