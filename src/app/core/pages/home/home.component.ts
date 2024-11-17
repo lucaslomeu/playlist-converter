e9881fef40ee1c498647851cb9929818b37dd92b
Lucas Lomeu <lucaslomeugomes@gmail.com>
2024-11-17 18:24:04 -0300

    remove unused codes home

--- src/app/core/pages/home/home.component.ts
+++ src/app/core/pages/home/home.component.ts
@@ -19,50 +19,7 @@ export class HomeComponent implements OnInit {
     private router: Router
   ) {}
 
-  ngOnInit() {
-    this.route.queryParams.subscribe((params) => {
-      const code = params['code'];
-      if (code) {
-        this.spotifyService
-          .getAccessToken(code)
-          .subscribe((res: AuthenticateResponse) => {
-            console.warn('RES', res);
-            this.spotifyService
-              .getPlaylists(res.access_token)
-              .subscribe((res) => {
-                console.warn('RES', res);
-              });
-          });
-      } else {
-        // Lidar com o erro ou redirecionar
-      }
-    });
-
-    this.route.fragment.subscribe((fragment: string | null) => {
-      if (fragment) {
-        const params = new URLSearchParams(fragment);
-        const accessToken = params.get('access_token');
-        const authCode = params.get('code');
-
-        if (accessToken) {
-          console.log('Access Token:', accessToken);
-          this.youtubeService.getPlaylists(accessToken).subscribe((res) => {
-            console.warn('PLAYLISTTTT', res);
-          });
-          // Aqui você pode armazenar o token e usá-lo para futuras requisições à API do YouTube
-        }
-
-        if (authCode) {
-          console.log('Authorization Code:', authCode);
-          // Aqui você pode enviar o código ao seu servidor para trocá-lo por um token
-        }
-      }
-    });
-  }
-
-  protected youtube() {
-    this.youtubeService.authenticate();
-  }
+  ngOnInit() {}
 
   protected getStarted() {
     this.router.navigateByUrl('/started');
